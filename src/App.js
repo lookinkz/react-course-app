cb1a682ac610b5a9676009957024555b73831f29
lookinkz <work@quana.kz>
2023-03-11 15:27:51 +0600

    17 video over

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import CostItem from "./components/CostItem";
+
 function App() {
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <CostItem />
     </div>
   );
 }
---
+++ src/components/CostItem.js
@@ -0,0 +1,5 @@
+function CostItem() {
+  return <h2>Элемент расхода</h2>;
+}
+
+export default CostItem;
