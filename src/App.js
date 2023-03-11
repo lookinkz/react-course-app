d594dc0a42ef6ba01c3e0cddde487e9f5c421e94
lookinkz <work@quana.kz>
2023-03-11 15:31:56 +0600

    18 video is over

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import CostItem from "./components/CostItem";
 function App() {
   return (
     <div>
+      <h1>ИЗУЧАЕМ REACT!!!</h1>
       <CostItem />
     </div>
   );
--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,5 +1,13 @@
 function CostItem() {
-  return <h2>Элемент расхода</h2>;
+  return (
+    <div>
+      <div>11 марта 2023 года</div>
+      <div>
+        <h2>Холодильник</h2>
+        <div>$999.99</div>
+      </div>
+    </div>
+  );
 }
 
 export default CostItem;
